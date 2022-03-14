cf05287ed9410447959cf0cb38e8d812a086c513
Wesley Victor <w3sleyv1ctor@gmail.com>
2022-03-14 14:26:34 -0300

    Ajuste

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public function getResults($name = null){
+
         if(!$name){
             return $this->get();
         }
         
         return $this->where('name', 'LIKE', "%{$name}%")->get();
+        
     }
 }
