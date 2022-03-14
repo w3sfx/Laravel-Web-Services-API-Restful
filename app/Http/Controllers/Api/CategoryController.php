21ae03fc2ce24d390ce754874eeb4028a8e91f20
Wesley Victor Tomáz da Silva <wesley.silva@engeselt.com.br>
2022-03-14 14:21:55 -0300

    Model / Tabela de Categorias, Listagem Categorias, Opção de Filtros

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index(Category $category, Request $request){
+        $categories = $category->getResults($request->name);
+
+        return response()->json($categories);
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public function getResults($name = null){
+        if(!$name){
+            return $this->get();
+        }
+        
+        return $this->where('name', 'LIKE', "%{$name}%")->get();
+    }
+}
