2c5ac693e9bba752587b27653c485efdd35da9e3
Wesley Victor <w3sleyv1ctor@gmail.com>
2022-03-15 13:47:49 -0300

    Cadastro de Categoria - Edição de Categoria - Validação de Cadastro Categoria

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,12 +5,41 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Http\Requests\StoreUpdateCategoryFormRequest;
 
 class CategoryController extends Controller
 {
+
+    private $category;
+
+    public function __construct(Category $category){
+        $this->category = $category;
+    }
+
     public function index(Category $category, Request $request){
-        $categories = $category->getResults($request->name);
+        $categories = $this->category->getResults($request->name);
 
         return response()->json($categories);
     }
+
+    public function store(StoreUpdateCategoryFormRequest $request){
+        $category = $this->category->create($request->all());
+
+        return response()->json($category, 201);
+    }
+
+    public function update(Request $request, $id) {
+
+        if (!$category = $this->category->find($id)) {
+
+            return response()->json(['error' => 'Not Found'], 404);
+
+        }
+
+        $category->update($request->all());        
+
+        return response()->json($category);
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,7 @@
 <?php
 
 $this->get('categories', 'Api\CategoryController@index');
+
+$this->post('categories', 'Api\CategoryController@store');
+
+$this->put('categories/{id}', 'Api\CategoryController@update');
